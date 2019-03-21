90ce2d2ad916ff876c3bb530a625d0afc873776e
Anchi Cheng <acheng@nysbc.org>
2019-03-20 20:58:16 -0700

    more verbose

--- resetdata.sh
+++ resetdata.sh
@@ -3,10 +3,17 @@ mysql -u root --silent --skip-column-names -e "SHOW TABLES" projectdb | xargs -L
 # remove ap* databases
 mysql -u root --silent -e "SHOW DATABASES" mysql | xargs -L1 -I {} bash -c "if [[ {} == 'ap'* ]]; then echo 'DROP DATABASE '{}; fi" | xargs -L1 -I% mysql -u root -v -e "%"
 
+echo 'REMOVE ALL DATA in /emg/data/leginon'
+echo ''
 rm -rf /emg/data/leginon/*
+echo 'REMOVE ALL DATA in /emg/data/appion'
+echo ''
 rm -rf /emg/data/appion/*
+echo 'REMOVE ALL DATA in /emg/data/frame'
+echo ''
 rm -rf /emg/data/frame/*
 
+echo 'REINITIALIZE DATABASES'
 mysql -u root leginondb < /sw/sql/leginondb.sql
 mysql -u root projectdb < /sw/sql/projectdb.sql
 
